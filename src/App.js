ccd54538977ed42a2ccac2404c8ff71e056511c7
Sherin Shelvan <sherinshelvan@gmail.com>
2019-12-11 11:48:13 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import Home from './pages/Home';
+import Admin from './pages/Admin';
+import { AuthContext } from "./context/auth";
+import PrivateRoute from './PrivateRoute';
+import Login from "./pages/Login";
+import Signup from './pages/Signup';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+function App(props) {
+  const [authTokens, setAuthTokens] = useState();
+  const setTokens = (data) => {
+    localStorage.setItem("tokens", data);
+    setAuthTokens(data);
+    console.log(data,  localStorage.getItem("tokens"));
+  }
+  return (    
+    <AuthContext.Provider value={{ authTokens, setAuthTokens: setTokens }}>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home Page</Link>
+          </li>
+          <li>
+            <Link to="/admin">Admin Page</Link>
+          </li>
+        </ul>
+        
+        <Route exact path="/" component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
+        <PrivateRoute path="/admin" component={Admin} />
+      </div>
+    </Router>    
+    </AuthContext.Provider>
   );
 }
 
